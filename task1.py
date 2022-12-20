b792cd16b1f1307e1384ce0101305e10922cdc7d
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-20 21:18:27 +0700

    Completed task 1

--- task1.py
+++ task1.py
@@ -30,3 +30,40 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    """
+    Класс для матриц
+    """
+    def __init__(self, matrix_list, description=''):
+        self.matrix_list = matrix_list
+        self.description = description
+
+    def __str__(self):
+        view_matrix = ''
+        if self.description:
+            view_matrix = f'{self.description}:\n{view_matrix}'
+        for i in self.matrix_list:
+            for j in i:
+                view_matrix += f'{j} '
+            view_matrix += '\n'
+        return view_matrix
+
+    def __add__(self, other):
+        self.description = 'Результирующая матрица'
+        first_length = len(self.matrix_list)
+        second_length = len(other.matrix_list)
+        for i in range(first_length):
+            for j in range(second_length):
+                self.matrix_list[i][j] += other.matrix_list[i][j]
+        return self.__str__()
+
+
+my_matrix_1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "Первая матрица")
+print(my_matrix_1)
+my_matrix_2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "Вторая матрица")
+print(my_matrix_2)
+print(my_matrix_1 + my_matrix_2)
+# my_matrix_1.description = "Первая матрица"
+# print(my_matrix_1)
